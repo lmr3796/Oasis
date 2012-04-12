54d97504cfd8a83ad91db02cd29dc00e750cff0b
yiping-wantoto <yiping@wantoto.com>
2012-04-13 02:40:57 +0800

    finish recent as well

--- src/com/android/Oasis/recent/Recent.java
+++ src/com/android/Oasis/recent/Recent.java
@@ -1,5 +1,10 @@
 package com.android.Oasis.recent;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
@@ -8,14 +13,23 @@ import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.android.Oasis.R;
 
 public class Recent extends Activity {
 
+	String type;
+	private static int NUM_RAIN = 3;
+	private static int NUM_WORM = 7;
+	private static int NUM_LEAF = 3;
+	private static int NUM_SICK = 3;
+
 	private ViewPager viewPager;
-	private static int NUM_VIEWS = 20;
 	private Context cxt;
 	private pagerAdapter pageradapter;
 
@@ -25,10 +39,47 @@ public class Recent extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.recent);
 		cxt = this;
+		type = "rain";
 
 		pageradapter = new pagerAdapter();
 		viewPager = (ViewPager) findViewById(R.id.pager);
 		viewPager.setAdapter(pageradapter);
+
+		ImageButton btn_rain = (ImageButton) findViewById(R.id.recent_btn_rain);
+		btn_rain.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				type = "rain";
+				viewPager.setAdapter(pageradapter);
+			}
+		});
+
+		ImageButton btn_worm = (ImageButton) findViewById(R.id.recent_btn_worm);
+		btn_worm.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				type = "worm";
+				viewPager.setAdapter(pageradapter);
+			}
+		});
+		
+		ImageButton btn_leaf = (ImageButton) findViewById(R.id.recent_btn_leaf);
+		btn_leaf.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				type = "leaf";
+				viewPager.setAdapter(pageradapter);
+			}
+		});
+		
+		ImageButton btn_sick = (ImageButton) findViewById(R.id.recent_btn_sick);
+		btn_sick.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				type = "sick";
+				viewPager.setAdapter(pageradapter);
+			}
+		});
 
 	}
 
@@ -36,7 +87,15 @@ public class Recent extends Activity {
 
 		@Override
 		public int getCount() {
-			return NUM_VIEWS;
+			if (type.equals("rain"))
+				return NUM_RAIN;
+			if (type.equals("worm"))
+				return NUM_WORM;
+			if (type.equals("leaf"))
+				return NUM_LEAF;
+			if (type.equals("sick"))
+				return NUM_SICK;
+			return NUM_RAIN;
 		}
 
 		/**
@@ -55,14 +114,51 @@ public class Recent extends Activity {
 		 */
 		@Override
 		public Object instantiateItem(View collection, int position) {
+
+			String pos = String.valueOf(position);
+			String filename = type + pos + ".txt";
+			String contentStr = "";
+
+			ScrollView sv = new ScrollView(cxt);
+
+			LinearLayout ll = new LinearLayout(cxt);
+			ll.setOrientation(LinearLayout.VERTICAL);
+
 			TextView tv = new TextView(cxt);
 			tv.setText("This is page # " + position);
 			tv.setTextColor(Color.WHITE);
 			tv.setTextSize(30);
 
-			((ViewPager) collection).addView(tv, 0);
+			ll.addView(tv);
 
-			return tv;
+			InputStream input;
+			try {
+				input = getAssets().open(filename);
+				if (input != null) {
+					// prepare the file for reading
+					InputStreamReader inputreader = new InputStreamReader(input);
+					BufferedReader buffreader = new BufferedReader(inputreader);
+					String line;
+
+					while ((line = buffreader.readLine()) != null) {
+						contentStr += line + '\n';
+					}
+					
+					TextView cont = new TextView(cxt);
+					cont.setTextColor(Color.WHITE);
+					cont.setTextSize(20);
+					cont.setText(contentStr);
+					ll.addView(cont);
+
+				}
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			sv.addView(ll);
+			((ViewPager) collection).addView(sv);
+			return sv;
 		}
 
 		/**
@@ -80,12 +176,12 @@ public class Recent extends Activity {
 		 */
 		@Override
 		public void destroyItem(View collection, int position, Object view) {
-			((ViewPager) collection).removeView((TextView) view);
+			((ViewPager) collection).removeView((ScrollView) view);
 		}
 
 		@Override
 		public boolean isViewFromObject(View view, Object object) {
-			return view == ((TextView) object);
+			return view == ((ScrollView) object);
 		}
 
 		/**
